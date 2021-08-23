bfa4dc4bd6da12a6803fa10ccc51cbf776613b56
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 22:39:21 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,18 +1,24 @@
+<head>
+  <link rel="stylesheet" href="style.css" />
+</head>
 <?php include_once 'Master_Data/header.php' ?>
 
 <div class="Index">
   <div style="width: 100%;">
     <h1 class="Titre">Trouve ton partenaire sportif</h1>
   </div>
-  <div>
-    <p>Tu adores un sport mais tu ne toruves jamais personne pour t'accomper, se site est fait pour toi !</p>
-    <p>Inscris-toi et recherche dès mainteant ton accolyte préféré</p>
+  <div class="IndexP">
+    <p>Tu adores un sport mais tu ne toruves jamais personne pour t'accomper, se site est fait pour toi !
+      Inscris-toi et recherche dès mainteant ton accolyte préféré</p>
+    <img class="imgHome" src="img/Home.jpg">
   </div>
-  <div class="imgHome"><img src="img/Home.jpg"></div>
   <br>
-  <?php
-  echo "<a class=\"bouton btn\" href=\"Page_ajout.php\" role=\"button\">Inscrivez-vous !</a>";
-  ?>
+  <div class="IndexBtn">
+    <?php
+    echo "<a class=\"bouton btn\" href=\"Page_ajout.php\" role=\"button\">Inscrivez-vous !</a>";
+    ?>
+  </div>
+</div>
 </div>
 
 <?php include_once 'Master_Data/footer.php' ?>
