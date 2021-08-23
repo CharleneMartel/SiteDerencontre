48b9c23ec840cb2c7929f1a035674ede60ad294e
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 15:48:19 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,11 +1,11 @@
 <?php include_once 'includes/header.php' ?>
 <div class="container">
-  <?php 
-    if (isset($_GET['logout'])) {
+  <?php
+  if (isset($_GET['logout'])) {
     echo "<div class=\"alert alert-success\" role=\"alert\">";
     echo "Vous vous êtes déconnecté";
     echo "</div>";
-    }
+  }
   ?>
   <div class="row" id="jumbo">
     <div class="jumbotron jumbotron-fluid">
@@ -14,14 +14,14 @@
         <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit placeat illo omnis, assumenda
           ullam tempora sunt eum vero fuga ipsam? Numquam minima optio officia aspernatur, minus corrupti! Doloremque,
           voluptate consequuntur?</p>
-        <?php 
+        <?php
         if (!isset($_COOKIE['email'])) {
-        echo "<a class=\"bouton btn\" href=\"inscription.php\" role=\"button\">Inscrivez-vous !</a>";
+          echo "<a class=\"bouton btn\" href=\"ajout.php\" role=\"button\">Inscrivez-vous !</a>";
         } else {
           echo "<a class=\"bouton btn\" href=\"recherche.php\" role=\"button\">Rechercher</a>";
         }
         ?>
-      </div>  
+      </div>
     </div>
   </div>
 </div>
