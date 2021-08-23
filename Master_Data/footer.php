0db1e4f8356bcc368c49d7978ac36e79f12d09ed
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 23:32:30 +0200

    Update footer.php

--- Master_Data/footer.php
+++ Master_Data/footer.php
@@ -1,28 +1,39 @@
-<body>
-  <footer class="footer">
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 
-    <div class="footer-links">
-      <div><a href="/index.php">Home</a></div>
-      <div><a href="/recherche.php">Recherche</a></div>
-      <div><a href="/compte.php">Mon compte</a></div>
-      <div><a href="/Page_ajout.php">Crée un compte</a></div>
-      <div><a href="/Page-connexion.php">Se connecter</a></div>
+<footer style="margin-top:20px;">
+  <h4 style="color: white;">Plan du site</h4>
+  <div class="menu" style="border-top:1px solid grey;">
+    <ul>
+      <li>
+        <a href="/index.php">Home</a>
+      </li>
+
+      <li>
+        <a href="/recherche.php">Recherche</a>
+      </li>
+
+      <li>
+        <a href="/compte.php">Mon compte</a>
+      </li>
+
+      <li>
+        <a href="/Page_ajout.php">Crée un compte</a>
+      </li>
+
+      <li>
+        <a href="/Page-connexion.php">Se connecter</a>
+      </li>
+
+    </ul>
+  </div>
+
+</footer>
+
+<div class="copyright">
+  <div class="container">
+    <div class="row text-center">
+      <p>Copyright © 2021 All rights reserved</p>
     </div>
-
-    <div class="footer-contact">
-
-      <div>Contact Us</div>
-      <div>
-        <form action="#" method="post">
-
-          <input type="text" name="email" placeholder="Email">
-          <textarea name="message" placeholder="Message"></textarea>
-          <button>Send</button>
-
-        </form>
-      </div>
-    </div>
-
-    <div>Company Name © 2021</div>
-  </footer>
-</body>
+  </div>
+</div>
