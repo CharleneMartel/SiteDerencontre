5f1a5dad9fa82ebd656efbfefbf47187f34cf3f5
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 22:10:37 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,19 +1,14 @@
 <?php include_once 'Master_Data/header.php' ?>
-<style>
-  .Index {
-    padding: 10px 30px !important;
-    text-align: center;
-  }
-</style>
+
 <div class="Index">
   <div style="width: 100%;">
-    <h1 class="">Trouve ton partenaire sportif</h1>
+    <h1 class="Titre">Trouve ton partenaire sportif</h1>
   </div>
   <div>
     <p>Tu adores un sport mais tu ne toruves jamais personne pour t'accomper, se site est fait pour toi !</p>
     <p>Inscris-toi et recherche dès mainteant ton accolyte préféré</p>
   </div>
-  <div style="width: 100%;"><img src="img/Home.jpg"></div>
+  <div class="imgHome"><img src="img/Home.jpg"></div>
   <br>
   <?php
   echo "<a class=\"bouton btn\" href=\"Page_ajout.php\" role=\"button\">Inscrivez-vous !</a>";
