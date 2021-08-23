987fc5aaeefdf59a7b894dd0e719d6ac9999dd59
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 19:20:59 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,21 +1,22 @@
 <?php include_once 'Master_Data/header.php' ?>
-<div class="container">
-  <?php
-  // if (isset($_GET['logout'])) {
-  //   echo "<div class=\"alert alert-success\" role=\"alert\">";
-  //   echo "Vous vous êtes déconnecté";
-  //   echo "</div>";
-  // }
-  ?>
-
-
-  <div class="row" id="jumbo">
+<style>
+  .Index {
+    padding: 10px 30px !important;
+    text-align: center;
+  }
+</style>
+<div class="Index">
+  <div style="width: 100%;">
     <h1 class="">Trouve ton partenaire sportif</h1>
-    <div style="width: 100%;"><img src="img/Home.jpg"></div>
-    <?php
-    echo "<a class=\"bouton btn\" href=\"Page_ajout.php\" role=\"button\">Inscrivez-vous !</a>";
-    ?>
   </div>
+  <div>
+    <p>Tu adores un sport mais tu ne toruves jamais personne pour t'accomper, se site ets fait pour toi !</p>
+    <p>Inscris-toi et recherche dès mainteant ton accolyte préféré</p>
+  </div>
+  <div style="width: 100%;"><img src="img/Home.jpg"></div>
+  <?php
+  echo "<a class=\"bouton btn\" href=\"Page_ajout.php\" role=\"button\">Inscrivez-vous !</a>";
+  ?>
 </div>
 
 <?php include_once 'Master_Data/footer.php' ?>
