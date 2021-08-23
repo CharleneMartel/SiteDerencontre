c321ec0284f9c64b4d84399b0a258f9f5513c765
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 17:44:48 +0200

    Update Page-connexion.php

--- Page-connexion.php
+++ Page-connexion.php
@@ -1,7 +1,7 @@
 <?php include_once 'Master_Data/header.php' ?>
 <div class="container form">
   <h2>Je me connecte</h2>
-  <form class="bg-light col-5" method="post" action="Procédures/connexion.php">
+  <form class="bg-light col-5" method="post" action="Pross/connexion.php">
     <?php
     if (isset($_GET['msg'])) {
       echo "<div class=\"form-group\">";
@@ -12,18 +12,18 @@
 
     <div class="form-group">
       <label name="email">Adresse e-mail</label>
-      <input type="email" name="email" class="form-control" placeholder="nom@exemple.com" required>
+      <input type="email" name="email" class="form-control" required>
     </div>
     <div class="form-group">
       <label name="mdp">Mot de passe</label>
-      <input type="password" name="mdp" class="form-control" placeholder="Mot de passe" required>
+      <input type="password" name="mdp" class="form-control" required>
     </div>
     <div class="form-group">
       <button type="submit" class="btn bouton">Se connecter</button>
     </div>
     <div class="form-group">
       <small class="text-muted">Si vous n'avez pas déjà un compte,
-        <a href="ajout.php">cliquez ici</a>.
+        <a href="Page_ajout.php">cliquez ici</a>.
       </small>
     </div>
   </form>
