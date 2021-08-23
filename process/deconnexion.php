1d4929f9c89eec2602b03eea1578f316d2eb51d3
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 17:28:03 +0200

    Delete deconnexion.php

--- process/deconnexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  session_unset();
-  session_destroy();
-  setcookie('email', '', time()-3600, '/', '', false, false);
-  setcookie('mdp', '', time()-3600, '/', '', false, false);
-  header("Location:../index.php?logout");
-?>
