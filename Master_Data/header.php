dd95f9025687b353f89bdf5f6796e28228154bc9
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 18:01:09 +0200

    Update header.php

--- Master_Data/header.php
+++ Master_Data/header.php
@@ -8,15 +8,15 @@ require 'database.php';
 <html>
 
 <head>
-  <title>Rencontres sports</title>
+  <title>Meeting for athletes</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="includes/style.css">
+  <link rel="stylesheet" href="Master_Data/style.css">
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
     <div class="container">
-      <a class="navbar-brand" href="index.php">Meetings Sportifs</a>
+      <a class="navbar-brand" href="index.php">Meeting for athletes</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
