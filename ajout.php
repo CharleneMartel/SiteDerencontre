36aac48013178b947cf3bb7da0cb1ce1b542f2e0
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 17:02:57 +0200

    Update ajout.php

--- ajout.php
+++ ajout.php
@@ -1,54 +1,68 @@
 <?php include_once 'includes/header.php' ?>
 <div class="container form">
     <h2>Bienvenue ! </h2>
-    <h4>Veuiller saisir vos informations ci dessous :</h4>
+    <div>
+        <small class="text-muted">Si vous avez déjà un compte,
+            <a href="connexion.php">cliquez ici</a>.
+        </small>
+    </div>
+    <h4>Sinon veuiller saisir vos informations ci dessous :</h4>
     <form class="" method="post" action="process/inscription_process.php" enctype="multipart/form-data">
         <div class="">
-            <label name="nom">Nom</label>
-            <input name="nom" class="form-control" required>
+            <div>
+                <label name="nom">Nom</label>
+                <input name="nom" class="form-control" required>
+            </div>
+            <div>
+                <label name="prenom">Prénom</label>
+                <input name="prenom" class="form-control" placeholder="Mathieu" required>
+            </div>
+            <div>
+                <label name="email">Mail </label>
+                <input type="email" name="email" class="form-control" placeholder="nom@exemple.com" required>
+            </div>
+            <div>
+                <label name="mdp">Mot de passe</label>
+                <input type="password" name="mdp" class="form-control" placeholder="Mot de passe" required>
+            </div>
+            <div>
+                <label name="ville">Ville</label>
+                <input name="ville" class="form-control" placeholder="Paris" required>
+            </div>
+            <div>
+                <label name="sport">Sport pratiqué</label>
+                <select class="form-control" name="sportPratique">
+                    <?php
+                    $sql = "SELECT `nom` from sports;";
+                    // foreach ($db->query($sql) as $ligne) {
+                    //    echo "<option>" . str_replace("_", " ", $ligne['nom']) . "</option>";
+                    //}
+                    ?>
+                </select>
+            </div>
+            <div>
+                <label name="niveau">Niveau</label>
+                <select class="form-control" name="niveau" required>
+                    <?php
+                    $sql = "SELECT `niveau` from niveaux_sportifs;";
+                    //foreach ($db->query($sql) as $ligne) {
+                    //     echo "<option>" . $ligne['niveau'] . "</option>";
+                    // }
+                    ?>
+                </select>
+            </div>
+            <div>
+                <label name="nouveauSport">Ajouter un sport si votre sport n'apparait pas dans la liste</label>
+            </div>
+            <div>
+                <input name="nouveauSport" class="form-control">
+            </div>
+            <div>
+                <button type="submit" class="btn bouton">S'inscrire</button>
+            </div>
 
-            <label name="prenom">PrÃ©nom</label>
-            <input name="prenom" class="form-control" placeholder="Mathieu" required>
-
-            <label name="email">Adresse e-mail</label>
-            <input type="email" name="email" class="form-control" placeholder="nom@exemple.com" required>
-
-            <label name="mdp">Mot de passe</label>
-            <input type="password" name="mdp" class="form-control" placeholder="Mot de passe" required>
-
-            <label name="ville">Ville</label>
-            <input name="ville" class="form-control" placeholder="Paris" required>
-
-            <label name="sport">Sport pratiqué</label>
-            <select class="form-control" name="sportPratique">
-                <?php
-                // $sql = "SELECT `nom` from sports;";
-                // foreach ($db->query($sql) as $ligne) {
-                //     echo "<option>" . str_replace("_", " ", $ligne['nom']) . "</option>";
-                // }
-                ?>
-            </select>
-
-            <label name=" niveau">Niveau</label>
-            <select class="form-control" name="niveau" required>
-                <?php
-                //$sql = "SELECT `niveau` from niveaux_sportifs;";
-                //foreach ($db->query($sql) as $ligne) {
-                //     echo "<option>" . $ligne['niveau'] . "</option>";
-                // }
-                ?>
-            </select>
-
-            <label name="nouveauSport">Ajouter un sport</label>
-            <small class="text-muted">Si votre sport pratiquÃ© n'est pas indiquÃ©</small>
-            <input name="nouveauSport" class="form-control">
-            <button type="submit" class="btn bouton">S'inscrire</button>
-
-            <small class="text-muted">Si vous avez déjà  un compte,
-                <a href="connexion.php">cliquez ici</a>.
-            </small>
         </div>
+
+    </form>
 </div>
-</form>
-</div>
-<?php include_once 'includes/footer.php' ?>
+<?php include_once 'Data/footer.php' ?>
