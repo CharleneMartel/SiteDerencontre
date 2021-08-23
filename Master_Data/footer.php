7e7272c2efa3a2cbef68970ee4a9b0b3e743c2c6
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-23 21:40:58 +0200

    Update footer.php

--- Master_Data/footer.php
+++ Master_Data/footer.php
@@ -2,47 +2,53 @@
   .footer {
     background-color: #9370DB;
     padding: 20px 30px 0;
-    border: solid;
-    display: flex;
-    margin: auto;
+    border: white;
+    display: inline-flex;
     width: 100%;
-    grid-auto-flow: row;
+    position: relative;
+    height: 15%;
   }
 
-  .footer-link a {
+  .footer-links {
+
     color: white;
     text-decoration: none;
+    margin-left: 25px;
   }
 
   .footer-contact {
     float: right;
     margin-bottom: 150px;
+    justify-content: center;
 
   }
 </style>
-<footer class="footer">
 
-  <div class="footer-links">
-    <div><a href="/index.php">Home</a></div>
-    <div><a href="/recherche.php">Recherche</a></div>
-    <div><a href="/compte.php">Mon compte</a></div>
-    <div><a href="/Page_ajout.php">Crée un compte</a></div>
-    <div><a href="/Page-connexion.php">Se connecter</a></div>
-  </div>
+<body>
+  <footer class="footer">
 
-  <div class="footer-contact">
-
-    <div>Contact Us</div>
-    <div>
-      <form action="#" method="post">
-
-        <input type="text" name="email" placeholder="Email">
-        <textarea name="message" placeholder="Message"></textarea>
-        <button>Send</button>
-
-      </form>
+    <div class="footer-links">
+      <div><a href="/index.php">Home</a></div>
+      <div><a href="/recherche.php">Recherche</a></div>
+      <div><a href="/compte.php">Mon compte</a></div>
+      <div><a href="/Page_ajout.php">Crée un compte</a></div>
+      <div><a href="/Page-connexion.php">Se connecter</a></div>
     </div>
-  </div>
 
-  <div>Company Name © 2021</div>
-</footer>
+    <div class="footer-contact">
+
+      <div>Contact Us</div>
+      <div>
+        <form action="#" method="post">
+
+          <input type="text" name="email" placeholder="Email">
+          <textarea name="message" placeholder="Message"></textarea>
+          <button>Send</button>
+
+        </form>
+      </div>
+    </div>
+
+    <div>Company Name © 2021</div>
+  </footer>
+</body>
