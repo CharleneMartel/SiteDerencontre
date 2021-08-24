20c8ba8e5a37a4dfaeced3b6c919f23f6c4d2653
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-24 13:07:33 +0200

    Update header.php

--- Master_Data/header.php
+++ Master_Data/header.php
@@ -3,9 +3,6 @@ session_start();
 require 'database.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-
 <head>
   <title>Rencontre pour sportif</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
