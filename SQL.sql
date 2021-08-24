3e1d37ca3141a1c34cc2eb05c6491c729f8fdec3
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-24 16:11:29 +0200

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -15,10 +15,7 @@ CREATE TABLE `utilisateurs` (
   `email` varchar(500) NOT NULL,
   `mot_de_passe` varchar(500) NOT NULL,
   `sport_pratique` varchar(20) NOT NULL,
-  `niveau` varchar(20) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
-  `last_login` datetime NOT NULL DEFAULT '1970-01-01 00:00:00'
+  `niveau` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 --Index
 ALTER TABLE `utilisateurs` ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`)
@@ -33,16 +30,14 @@ ALTER TABLE `utilisateurs` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INC
 CREATE TABLE `sports` (
   `id` int(11) NOT NULL,
   `nom` varchar(100) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:01'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- Insérer les données
-INSERT INTO `sports` (`id`, `nom`, `created_at`, `updated_at`) VALUES
-(1, 'Football', '2020-11-08 19:39:42', '1970-01-01 00:00:01'),
-(2, 'Basketball', '2020-11-08 19:40:00', '1970-01-01 00:00:01'),
-(3, 'Rugby', '2020-11-08 19:40:43', '1970-01-01 00:00:01'),
-(4, 'Natation', '2020-11-08 19:41:33', '1970-01-01 00:00:01'),
-(5, 'Judo', '2020-11-08 19:41:57', '1970-01-01 00:00:01');
+INSERT INTO `sports` (`id`, `nom`) VALUES
+(1, 'Football'),
+(2, 'Basketball'),
+(3, 'Rugby'),
+(4, 'Natation'),
+(5, 'Judo');
 --Clé primaire
 ALTER TABLE `sports` ADD PRIMARY KEY (`id`)
 --Auto 
@@ -53,15 +48,13 @@ ALTER TABLE `sports` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT
 --Table NiveauSportif
 CREATE TABLE `niveaux_sportifs` (
   `id` int(11) NOT NULL,
-  `niveau` varchar(20) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00'
+  `niveau` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- Insérer les données
-INSERT INTO `niveaux_sportifs` (`id`, `niveau`, `created_at`, `updated_at`) VALUES
-(1, 'débutant', '2020-11-08 20:31:48', '1970-01-01 00:00:00'),
-(2, 'confirmé', '2020-11-08 20:31:48', '1970-01-01 00:00:00'),
-(3, 'pro', '2020-11-08 20:32:03', '1970-01-01 00:00:00');
+INSERT INTO `niveaux_sportifs` (`id`, `niveau`) VALUES
+(1, 'débutant'),
+(2, 'confirmé'),
+(3, 'pro');
 --Clé primaire
 ALTER TABLE `niveaux_sportifs` ADD PRIMARY KEY (`id`)
 --Auto 
