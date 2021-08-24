e9311aa847360bc5957397ea60cb086be3bf8ca4
CharleneMartel <89074088+CharleneMartel@users.noreply.github.com>
2021-08-24 10:01:30 +0200

    Update footer.php

--- Master_Data/footer.php
+++ Master_Data/footer.php
@@ -1,6 +1,3 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-
 <footer style="margin-top:20px;">
   <h4 style="color: white;">Plan du site</h4>
   <div class="menu" style="border-top:1px solid grey;">
@@ -29,7 +26,6 @@
   </div>
 
 </footer>
-
 <div class="copyright">
   <div class="container">
     <div class="row text-center">
